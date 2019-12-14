acad26840c06bf3b007f64864c8a47515f5ef265
tomoyan819 <nakamura_tomoya@cam.inc.co.jp>
2019-12-14 18:20:10 +0900

    routerを作成

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,26 @@
-import './main/index.js';
-import './test/index.js';
-
 import { LitElement, html } from 'lit-element';
+import router from '../router';
 
 class TodoAppCompornent extends LitElement {
+
+  firstUpdated() {
+    window.Promise.resolve()
+      .then(() => router)
+      .then(() =>
+        router.init({outlet: this.shadowRoot.querySelector('.outlet')})
+      )
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
   render() {
     return html`
       <div class="header">
         <a href="/">メイン</a>
         <a href="/test">テスト</a>
       </div>
-      <div id="main"></div>
+      <div class="outlet"></div>
     `
   }
 };
